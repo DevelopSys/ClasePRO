4a644f08dbf5d8ac552def3ea704926a14df1bf8
Develop and System <37659043+DevelopSys@users.noreply.github.com>
2020-10-01 13:04:22 +0200

    update

---
+++ T2/codigos2021/T2_Inicio/src/Entrada.java
@@ -0,0 +1,23 @@
+public class Entrada {
+
+    // esta parte vale para algo
+    // esta es otra linea
+
+    /*Linea uno del comentario
+     * Linea dos del comentario
+     * */
+
+    /**
+     * @param : paramero1
+     * @author: Borja Martin
+     * @version: 1.0
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hola, primer programa en Java");
+        System.out.println("Segunda linea del programa");
+        System.out.println("Segunda linea del programa");
+        System.out.println("Segunda linea del programa");
+    }
+
+}
---
+++ T2/codigos2021/T2_Inicio/src/NuevaEntrada.java
@@ -0,0 +1,7 @@
+public class NuevaEntrada {
+
+    public static void main(String[] args){
+        System.out.println("Otra ejecucion del programa");
+    }
+
+}
