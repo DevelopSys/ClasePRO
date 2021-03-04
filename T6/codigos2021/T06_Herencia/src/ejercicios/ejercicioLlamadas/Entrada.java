d10993299ae81cb081f75e2c599f4c8f2da483f9
Develop and System <37659043+DevelopSys@users.noreply.github.com>
2021-03-04 13:02:01 +0100

    update

--- T6/codigos2021/T06_Herencia/src/ejercicios/ejercicioLlamadas/Entrada.java
+++ T6/codigos2021/T06_Herencia/src/ejercicios/ejercicioLlamadas/Entrada.java
@@ -6,8 +6,8 @@ public class Entrada {
         //Llamada llamada = new Llamada();
         //LlamadaLocal llamadaLocal = new LlamadaLocal(123,123,40);
         //llamadaLocal.mostrarDatos();
-        LlamadaProvincial llamadaProvincial = new LlamadaProvincial(123,123,40);
-        llamadaProvincial.mostrarDatos();
+        //LlamadaProvincial llamadaProvincial = new LlamadaProvincial(123,123,40);
+        //llamadaProvincial.mostrarDatos();
         //LlamadaNacional llamadaNacional = new LlamadaNacional(123,123,40,3);
         //llamadaNacional.mostrarDatos();
     }
