b25b55e76b1f6c5f81c23d681e846cf5a8949d0b
Develop and System <developandsystem@gmail.com>
2021-04-15 13:00:49 +0200

    github

--- T7/codigos2021/T07_Explicacion/src/ejerciciotorneo/Entrada.java
+++ T7/codigos2021/T07_Explicacion/src/ejerciciotorneo/Entrada.java
@@ -1,9 +1,26 @@
 package ejerciciotorneo;
 
+import java.util.ArrayList;
+
 public class Entrada {
 
     public static void main(String[] args) {
-        Torneo torneo = new Torneo();
-        torneo.mostrarClasificacion();
+
+        //Torneo torneo = new Torneo("La Liga");
+        Torneo.Equipo madrid = new Torneo.Equipo("Real Madrid");
+        Torneo.Equipo barcelona = new Torneo.Equipo("FC Barcelona");
+        Torneo.Equipo sevilla = new Torneo.Equipo("Sevilla FC");
+        Torneo.Equipo getafe = new Torneo.Equipo("Getafe FC");
+        ArrayList<Torneo.Equipo> listaEquipos = new ArrayList<>();
+        listaEquipos.add(madrid);
+        listaEquipos.add(barcelona);
+        listaEquipos.add(sevilla);
+        listaEquipos.add(getafe);
+        Torneo torneo = new Torneo("La liga",listaEquipos);
+        torneo.realizarSorteo();
+        //System.out.println(torneo.getListaParticipantes().size());
+        //Torneo.Partido partido = torneo.new Partido();
+        //Torneo.Equipo equipo = new Torneo.Equipo();
+
     }
 }
--- T7/codigos2021/T07_Explicacion/src/ejerciciotorneo/Torneo.java
+++ T7/codigos2021/T07_Explicacion/src/ejerciciotorneo/Torneo.java
@@ -1,36 +1,201 @@
 package ejerciciotorneo;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 
 public class Torneo {
 
+
+    private ArrayList<Partido> listaPartidos;
     private ArrayList<Equipo> listaParticipantes;
+    private String nombreTorneo;
+
+    public Torneo(String nombreTorneo, ArrayList<Equipo> listaParticipantes) {
+        this.listaParticipantes = listaParticipantes;
+        this.nombreTorneo = nombreTorneo;
+    }
+
+    public Torneo(String nombreTorneo) {
+        this.nombreTorneo = nombreTorneo;
+        this.listaParticipantes = new ArrayList<>();
+    }
 
     public Torneo() {
         this.listaParticipantes = new ArrayList<>();
-        listaParticipantes.add(new Equipo("Sevilla",60,35));
-        listaParticipantes.add(new Equipo("Barsa",65,56));
-        listaParticipantes.add(new Equipo("Atleti",67,50));
-        listaParticipantes.add(new Equipo("Madrid",66,58));
     }
 
-    public void mostrarClasificacion(){
 
-
-        Collections.sort(listaParticipantes, new Comparator<Equipo>() {
-            @Override
-            public int compare(Equipo o1, Equipo o2) {
-                return o2.getNombre().compareTo(o1.getNombre());
-            }
-        });
-
-
-
-        for (Equipo itemEquipo:listaParticipantes) {
-            System.out.printf("%s puntos: %d goles: %d%n",itemEquipo.getNombre(), itemEquipo.getPuntos(), itemEquipo.getGoles());
+    private void mostrarPartidos(){
+        for ( Partido itemPartido  : listaPartidos ) {
+            System.out.printf("%s vs %s%n",itemPartido.getEquipoLocal().getNombre(),itemPartido.getEquipoVisitante().getNombre());
         }
     }
+
+    private void mecanicaTorneo(){
+
+        listaPartidos = new ArrayList<>();
+        // MADRID ; BARSA ; SEVILLA ; GETAFE
+        // m - b  m - s m -g
+        // b - s b -g
+        // s - g
+
+        // IDA
+        for (int i = 0; i < listaParticipantes.size()-1; i++) {
+            //System.out.println(listaParticipantes.get(i).getNombre());
+            for (int j = i+1; j < listaParticipantes.size(); j++) {
+                //System.out.println("\t"+listaParticipantes.get(j).getNombre());
+                Partido partido = new Partido(listaParticipantes.get(i),listaParticipantes.get(j));
+                listaPartidos.add(partido);
+            }
+        }
+
+        // VUELTA
+        System.out.println("Realizando vuleta");
+        for (int i = listaParticipantes.size()-1; i > 0; i--) {
+            //System.out.println(listaParticipantes.get(i).getNombre());
+            for (int j = i-1; j >= 0 ; j--) {
+                //System.out.println("\t"+listaParticipantes.get(j).getNombre() );
+                Partido partido = new Partido(listaParticipantes.get(i), listaParticipantes.get(j));
+                listaPartidos.add(partido);
+            }
+        }
+
+        System.out.println("El número de  de partidos es: "+listaPartidos.size());
+        for (int i = 0; i < 10; i++) {
+            Collections.shuffle(listaPartidos);
+        }
+        mostrarPartidos();
+    }
+
+    public void realizarSorteo(ArrayList<Equipo> equipos){
+        if (equipos!=null) {
+            this.listaParticipantes = equipos;
+            // TODO realizar el sorteo con el for anidado del otro día
+            mecanicaTorneo();
+        } else {
+            System.out.println("Poro favor comprueba el funcionamiento");
+        }
+    }
+    public void realizarSorteo(){
+        // TODO realizar el sorteo con el for anidado del otro día
+        if (listaParticipantes.size()>0){
+            mecanicaTorneo();
+        } else {
+            System.out.println("Por favor revisa la forma de hacer el sorteo");
+        }
+    }
+
+    public ArrayList<Partido> getListaPartidos() {
+        return listaPartidos;
+    }
+
+    public void setListaPartidos(ArrayList<Partido> listaPartidos) {
+        this.listaPartidos = listaPartidos;
+    }
+
+    public ArrayList<Equipo> getListaParticipantes() {
+        return listaParticipantes;
+    }
+
+    public void setListaParticipantes(ArrayList<Equipo> listaParticipantes) {
+        this.listaParticipantes = listaParticipantes;
+    }
+
+    public String getNombreTorneo() {
+        return nombreTorneo;
+    }
+
+    public void setNombreTorneo(String nombreTorneo) {
+        this.nombreTorneo = nombreTorneo;
+    }
+
+    class Partido{
+
+        private Torneo.Equipo EquipoLocal, EquipoVisitante;
+        private boolean jugado;
+        private int golesLocal, golesVisitante;
+
+        public Partido(Equipo equipoLocal, Equipo equipoVisitante) {
+            EquipoLocal = equipoLocal;
+            EquipoVisitante = equipoVisitante;
+        }
+
+        public Partido() {
+        }
+
+        public Equipo getEquipoLocal() {
+            return EquipoLocal;
+        }
+
+        public void setEquipoLocal(Equipo equipoLocal) {
+            EquipoLocal = equipoLocal;
+        }
+
+        public Equipo getEquipoVisitante() {
+            return EquipoVisitante;
+        }
+
+        public void setEquipoVisitante(Equipo equipoVisitante) {
+            EquipoVisitante = equipoVisitante;
+        }
+
+        public boolean isJugado() {
+            return jugado;
+        }
+
+        public void setJugado(boolean jugado) {
+            this.jugado = jugado;
+        }
+
+        public int getGolesLocal() {
+            return golesLocal;
+        }
+
+        public void setGolesLocal(int golesLocal) {
+            this.golesLocal = golesLocal;
+        }
+
+        public int getGolesVisitante() {
+            return golesVisitante;
+        }
+
+        public void setGolesVisitante(int golesVisitante) {
+            this.golesVisitante = golesVisitante;
+        }
+    }
+
+    static class Equipo{
+
+        private String nombre;
+        private int puntos, goles;
+
+        public Equipo(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public int getPuntos() {
+            return puntos;
+        }
+
+        public void setPuntos(int puntos) {
+            this.puntos = puntos;
+        }
+
+        public int getGoles() {
+            return goles;
+        }
+
+        public void setGoles(int goles) {
+            this.goles = goles;
+        }
+    }
+
 }
