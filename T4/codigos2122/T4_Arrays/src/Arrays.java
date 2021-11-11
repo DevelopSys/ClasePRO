b53213ea90e9d89b92f4f034b70499df5916fce5
Develop and System <developandsystem@gmail.com>
2021-11-11 11:30:19 +0100

    update

---
+++ T4/codigos2122/T4_Arrays/src/Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // array de numeros
+        int[] numeros = new int[10];
+        boolean[] aciertos = new boolean[5];
+        String[] palabras = new String[3];
+        double[] decimales = new double[2];
+        int[] numerosDos = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int longitudArray = numerosDos.length;
+        //System.out.println(longitudArray);
+        System.out.println(numeros[3]);
+    }
+
+}
