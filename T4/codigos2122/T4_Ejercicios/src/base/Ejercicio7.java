c1ac3532c9d84cab9a2e01d8075fb4456ee7f591
Develop and System <developandsystem@gmail.com>
2021-11-15 11:17:06 +0100

    upadate

---
+++ T4/codigos2122/T4_Ejercicios/src/base/Ejercicio7.java
@@ -0,0 +1,13 @@
+package base;
+
+public class Ejercicio7 {
+
+    // Crea un programa que pida 10 palabras por consola y las guarde en un array de String.
+    // Una vez hecho esto saldrá un menú que me dará las siguientes opciones:
+    // Ver todas las palabras: mostrarla todas las palabras del array
+    // Obtener una palabra al azar: mostrará una palabra de las que existen en el array
+    // Ver número de letras: mostrará el número de letras que hay en total entre todas las palabras
+    // Ver media de letras: mostrará el número medio de letras entre todas las palabas
+    // Ver palabra con más letras
+    // Ver palabra con menos letras
+}
