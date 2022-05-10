1fecf50089f5199a32091971868561f3e03ee40f
Develop and System <developandsystem@gmail.com>
2022-05-10 11:02:35 +0200

    update

--- codigos2122/T9BD/src/main/java/Main.java
+++ codigos2122/T9BD/src/main/java/Main.java
@@ -1,15 +1,36 @@
 import controller.ControllerBD;
 import database.SchemeDB;
+import model.Alumno;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String nombre = null, apellido = null, continuar = null;
+        int edad = 0;
         ControllerBD controllerBD = new ControllerBD();
-        controllerBD.insertarAlumnoStatement();
+        //controllerBD.insertarAlumnoStatement();
+        controllerBD.insertarAlumnoPrepare();
+        /*
+        do {
+            System.out.println("Intro nombre");
+            nombre = scanner.next();
+            System.out.println("Intro apellido");
+            apellido = scanner.next();
+            System.out.println("Intro edad");
+            edad = scanner.nextInt();
+            Alumno alumno = new Alumno(nombre, apellido, edad);
+            controllerBD.insertarAlumnoStatement(alumno);
 
+            System.out.println("Quieres continuar(s/n)");
+            continuar = scanner.next();
+
+        } while (continuar.equalsIgnoreCase("s"));
+           */
 
     }
 }
--- codigos2122/T9BD/src/main/java/controller/ControllerBD.java
+++ codigos2122/T9BD/src/main/java/controller/ControllerBD.java
@@ -1,17 +1,17 @@
 package controller;
 
 import database.SchemeDB;
+import model.Alumno;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class ControllerBD {
 
     private Connection conn;
     // no comprueba tipos
     private Statement statement;
+    // comprueba tipos de datos
+    private PreparedStatement preparedStatement;
 
 
     private void getConnection(){
@@ -67,19 +67,73 @@ public class ControllerBD {
             closeConnection();
         }
     }
+    public void insertarAlumnoStatement(Alumno alumno) {
 
+        // INSERT INTO alumnos (nombre, apellido, edad) VALUES ('BORJA','MARTIN',38)
+        try {
+            getConnection();
+            statement = conn.createStatement();
+
+            String queryFormat = String.format("INSERT INTO %s (%s, %s, %s) VALUES ('%s','%s',%d)",SchemeDB.TAB_ALU,
+                    SchemeDB.COL_NOMBRE,SchemeDB.COL_APELLIDO,SchemeDB.COL_EDAD,
+                    alumno.getNombre(),alumno.getApellido(),alumno.getEdad());
+            //int numeroRow = statement.executeUpdate(queryFormat);
+            if (statement.executeUpdate(queryFormat)>0){
+                System.out.println("Alumno insertado correctamente");
+            }
+            //System.out.println(numeroRow);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            closeConnection();
+        }
+    }
     // Desde la entrada, pedir al usuario datos para introducir alumnos (recursivo)
     // el alumno se mete en base de datos
     // mediante un modelo
+    public void insertarAlumnoPrepare(){
+        String nombre = "Borja";
+        String apellido = "Martin";
+        int  edad = 38;
+        String query  =  "INSERT INTO alumnos (nombre, apellido, edad) VALUES (?,?,?)";
+        try {
+            // no ejecuta la query --> la deja preparada
+            getConnection();
+            preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1,nombre);
+            preparedStatement.setString(2,apellido);
+            preparedStatement.setInt(3,edad);
+            preparedStatement.executeUpdate();
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            closeConnection();
+        }
 
-
-
+    }
     public Connection getConn() {
         return conn;
     }
-
     public void setConn(Connection conn) {
         this.conn = conn;
     }
+
+    // modificar la edad de un usuario. Para ello el sistema pedira el nombre del usuario
+    // y la nueva edad del mismo. Una vez realizada la modificacion el sistema
+    // avisará del numero de usuarios que ha cambiado SE HAN ACTUALIZADO X USUARIOS
+
+    // borrar los usuarios. Para ello el sistema pedira una edad y borrá todos aquellos
+    // usuarios que tengan una edad inferior a la pedida por el sistema
+    // el sistema avisará del numero de usuarios borrados
 }
