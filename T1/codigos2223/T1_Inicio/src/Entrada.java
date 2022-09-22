22ef028b3496d03b22e652cb070874d5eaac8eed
Develop and System <developandsystem@gmail.com>
2022-09-22 10:44:47 +0200

    update

---
+++ T1/codigos2223/T1_Inicio/src/Entrada.java
@@ -0,0 +1,13 @@
+public class Entrada {
+
+
+    public static void main (String [] args){
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+    }
+
+}
