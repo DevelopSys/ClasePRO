17fa9801b71dfa532470c97518bdd2132a4d5fbe
Develop and System <developandsystem@gmail.com>
2022-09-30 12:31:37 +0200

    update

---
+++ T2/codigos2223/T2_Operadores/src/prueba/ClasePrueba.java
@@ -0,0 +1,25 @@
+package prueba;
+
+public class ClasePrueba {
+
+    // protected
+    String variableDefecto;
+    private String variablePrivada;
+
+    public ClasePrueba(){
+
+    }
+
+    public void metodoPublico(){
+
+    }
+
+    private void metodoPrivado(){
+
+    }
+
+    public static void metodoEstatico(){
+
+    }
+
+}
