1e062bcddd1b24b01caa1787966aeaf316ea0da1
Develop and System <developandsystem@gmail.com>
2023-03-15 12:22:32 +0100

    update

--- T8/codigos2223/T8Ficheros/T8Ficheros/src/Entrada.java
+++ T8/codigos2223/T8Ficheros/T8Ficheros/src/Entrada.java
@@ -1,9 +1,15 @@
+import java.io.File;
+
 public class Entrada {
 
     public static void main(String[] args) {
         OperacionesFicheros operacionesFicheros = new OperacionesFicheros();
         //operacionesFicheros.creacionFichero();
         //operacionesFicheros.informacionFichero();
-        operacionesFicheros.lecturaRuta();
+        //operacionesFicheros.lecturaRuta();
+        //operacionesFicheros.leerDirectorio(new File("/"));
+        //operacionesFicheros.leerFicheros();
+        //operacionesFicheros.leerBufferFicheros();
+        //operacionesFicheros.lecturaCodigos();
     }
 }
--- T8/codigos2223/T8Ficheros/T8Ficheros/src/OperacionesFicheros.java
+++ T8/codigos2223/T8Ficheros/T8Ficheros/src/OperacionesFicheros.java
@@ -1,17 +1,16 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class OperacionesFicheros {
 
-    public void metodoRecursivo(){
+    public void metodoRecursivo() {
         System.out.println("Ejecicion de un metodo recursivo");
         metodoRecursivo();
     }
 
-    public void creacionFichero(){
+    public void creacionFichero() {
         File file = new File("/Users/borjam/Desktop/ficheros");
         File fileFinal = new File("/Users/borjam/Desktop/ficheros/fichero2.txt");
         File fileCarpeta = new File("/Users/borjam/Desktop/ficheros/subcarpeta");
@@ -35,28 +34,131 @@ public class OperacionesFicheros {
         System.out.println(esCarpeta);
 
     }
-    public void informacionFichero(){
+
+    public void informacionFichero() {
         Scanner scanner = new Scanner(System.in);
         String rutaEntrada = scanner.next();
-        System.out.println("src/ficheros/"+rutaEntrada);
+        System.out.println("src/ficheros/" + rutaEntrada);
         Path path = Paths.get(rutaEntrada);
         /*File file = new File(String.valueOf(path));
         System.out.println(file.getName());
         System.out.println(file.canExecute());
         System.out.println(file.canWrite());*/
     }
-    public void lecturaRuta(){
+
+    public void lecturaRuta() {
         File file = new File("src/ficheros");
         System.out.println(file.getName());
         File[] listaNombres = file.listFiles();
         for (File fichero : listaNombres) {
             System.out.println(fichero.getName());
-            if (fichero.isDirectory()){
+            if (fichero.isDirectory()) {
                 for (File fichero1 : fichero.listFiles()) {
-                    System.out.println("\t"+fichero1.getName());
+                    System.out.println("\t" + fichero1.getName());
                 }
             }
         }
     }
 
+    public void leerDirectorio(File file) {
+
+        File[] ficheros = file.listFiles();
+        for (File i : ficheros) {
+            System.out.println(i.getName());
+            if (i.isDirectory()) {
+                System.out.print("\t");
+                leerDirectorio(i);
+            }
+        }
+
+    }
+
+    public void leerFicheros() {
+        File fichero = new File("src/ficheros/lectura.txt");
+        FileReader reader = null;
+        try {
+            reader = new FileReader(fichero);
+            int codigo = -1;
+            while ((codigo = reader.read()) != -1) {
+                System.out.print((char) codigo);
+            }
+        } catch (IOException e) {
+            System.out.println("Fichero no encontrado");
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException | NullPointerException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    public void leerBufferFicheros() {
+        File file = new File("src/ficheros/lectura.txt");
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+            fileReader = new FileReader(file);
+            bufferedReader = new BufferedReader(fileReader);
+            String linea = null;
+            StringBuilder contenido = new StringBuilder();
+            while ((linea = bufferedReader.readLine()) != null) {
+                contenido.append(linea + "\n");
+            }
+
+            System.out.println("CONTENIDO ES: ");
+            System.out.println(contenido);
+
+            // Cuantas lineas tiene un fichero
+            // Cuantas palabras tiene cada linea
+            // Cuantas letras tiene cada linea
+            // Cuantas palabras hay en total
+            // Cuantas letras hay en total
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                bufferedReader.close();
+            } catch (IOException | NullPointerException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    public void ejercicioConteo() {
+        // cuantas palabras
+        // cuantas letras
+        // cuantas lineas
+
+        int numeroLineas = 0;
+        int numeroPalabras = 0;
+        int numeroLetras = 0;
+
+        File file = new File("src/ficheros/lectura.txt");
+        FileReader reader = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+            reader = new FileReader(file);
+            bufferedReader = new BufferedReader(reader);
+            String linea = null; // desde 0 lineas a m linea
+
+            // si hay linea
+            while ((linea = bufferedReader.readLine()) != null){
+                //System.out.println(linea);
+                numeroLineas++;
+            }
+
+            System.out.println("El numero de lineas es: "+numeroPalabras);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
