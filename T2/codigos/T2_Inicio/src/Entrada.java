f640687dc8a2776a32a1ae77b8cf1acd715c1145
Develop and System <developandsystem@gmail.com>
2023-09-14 11:05:36 +0200

    update

---
+++ T2/codigos/T2_Inicio/src/Entrada.java
@@ -0,0 +1,16 @@
+public class Entrada {
+
+    // Esto es un comentario de linea simple
+    public static void main(String[] args) {
+
+        String nombre = "Borja";
+        int edad = 18;
+
+        System.out.printf("Mi nombre es %s y tengo %d años\n",nombre,edad);
+        System.out.println("Mi nombre es "+nombre+" y tengo "+edad+" años");
+        System.out.print("Mi edad es "+edad);
+
+    }
+}
+
+
