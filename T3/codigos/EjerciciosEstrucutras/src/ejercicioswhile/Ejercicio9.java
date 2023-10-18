4966d4765e90a436ae57e79ca0f92487068f96af
Develop and System <developandsystem@gmail.com>
2023-10-18 15:59:43 +0200

    Update Ejercicio9.java

--- T3/codigos/EjerciciosEstrucutras/src/ejercicioswhile/Ejercicio9.java
+++ T3/codigos/EjerciciosEstrucutras/src/ejercicioswhile/Ejercicio9.java
@@ -17,7 +17,7 @@ public class Ejercicio9 {
         // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
         // c o r r e o @ g m a i l . c o m
         palabra = palabra.substring(0,palabra.charAt('@'));
-        
+
 
         System.out.println(palabra);
 
