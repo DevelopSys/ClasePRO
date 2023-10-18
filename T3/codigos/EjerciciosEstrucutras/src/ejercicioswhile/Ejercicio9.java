e2ba39bdca4e6e90776d7cd55f6aec787f333743
Develop and System <developandsystem@gmail.com>
2023-10-18 11:11:08 +0200

    Update Ejercicio9.java

--- T3/codigos/EjerciciosEstrucutras/src/ejercicioswhile/Ejercicio9.java
+++ T3/codigos/EjerciciosEstrucutras/src/ejercicioswhile/Ejercicio9.java
@@ -17,64 +17,70 @@ public class Ejercicio9 {
             opcion = scanner.nextInt();
             switch (opcion) {
                 case 1:
-                    // JUNTAR LOS TRES CAMBIOS EN UN METODO
-                    System.out.println("Que numero quieres pasar a base 2");
-                    int numero = scanner.nextInt(); // 32
-                    String resultadoCambio = "";
-                    while (numero > 1) {
-                        int resto = numero % 2;
-                        resultadoCambio = resto + resultadoCambio;
-                        numero = numero / 2;
-                    }
-                    resultadoCambio = numero + resultadoCambio;
-                    System.out.println("El resultado de cambiar de base es " + resultadoCambio);
-
-                    // calcular base 2;
+                    Ejercicio9.cambiarBase(2);
                     break;
                 case 2:
-                    System.out.println("Que numero quieres pasar a base 8");
-                    numero = scanner.nextInt(); // 32
-                    resultadoCambio = "";
-                    while (numero > 7) {
-                        int resto = numero % 8;
-                        resultadoCambio = resto + resultadoCambio;
-                        numero = numero / 8;
-                    }
-                    resultadoCambio = numero + resultadoCambio;
-                    System.out.println("El resultado de cambiar de base es " + resultadoCambio);
-                    // calcular base 8;
+                    Ejercicio9.cambiarBase(8);
                     break;
                 case 3:
-                    // calcular base 16;
-                    System.out.println("Que numero quieres pasar a base 8");
-                    numero = scanner.nextInt(); // 32
-                    resultadoCambio = "";
-                    while (numero > 15) {
-                        int resto = numero % 16;
-                        switch (resto) {
-                            case 10:
-                                break;
-                            case 11:
-                                break;
-                            case 12:
-                                break;
-                            case 13:
-                                break;
-                            case 14:
-                                break;
-                            case 15:
-                                break;
-                        }
-                        resultadoCambio = resto + resultadoCambio;
-                        numero = numero / 16;
-                    }
-                    resultadoCambio = numero + resultadoCambio;
-                    System.out.println("El resultado de cambiar de base es " + resultadoCambio);
-                    // calcular base 8;
+                    Ejercicio9.cambiarBase(16);
+
                     break;
                 case 4:
                     break;
             }
         } while (opcion != 5);
     }
+
+    public static void cambiarBase(int base) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Que numero quieres pasar a base " + base);
+        int numero = scanner.nextInt(); // 32
+        String resultadoCambio = "";
+        // numero> b-1
+        while (numero > base - 1) {
+            int resto = numero % base;
+            // switch
+            String letra = "";
+            if (base == 16) {
+                switch (resto) {
+                    case 10:
+                        letra = "A";
+                        break;
+                    case 11:
+                        letra = "B";
+                        break;
+                    case 12:
+                        letra = "C";
+                        break;
+                    case 13:
+                        letra = "D";
+                        break;
+                    case 14:
+                        letra = "E";
+                        break;
+                    case 15:
+                        letra = "F";
+                        break;
+
+                }
+                if (resto > 9) {
+                    resultadoCambio = letra + resultadoCambio;
+                } else {
+                    resultadoCambio = resto + resultadoCambio;
+                }
+            }
+
+            if (base == 2 || base == 8) {
+                resultadoCambio = resto + resultadoCambio;
+            }
+
+            numero = numero / base;
+        }
+        resultadoCambio = numero + resultadoCambio;
+        System.out.println("El resultado de cambiar de base es " + resultadoCambio);
+
+    }
+
+
 }
