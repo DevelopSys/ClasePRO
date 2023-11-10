ba4d0858632c271f99d59c90bc1974a05ff06bc7
Develop and System <developandsystem@gmail.com>
2023-11-10 14:58:06 +0100

    Update Ejercicio5.java

--- T4/codigos/Ejercicios/src/arraylistbase/Ejercicio5.java
+++ T4/codigos/Ejercicios/src/arraylistbase/Ejercicio5.java
@@ -1,9 +1,6 @@
 package arraylistbase;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Objects;
+import java.util.*;
 
 public class Ejercicio5 {
 
@@ -11,8 +8,15 @@ public class Ejercicio5 {
 
     public void iniciarEjercicio(){
         iniciarLista();
-        //mostrarCoches();
-        filtrarDatos("ford");
+        reprogramarCoches("ford",50);
+        mostrarCoches();
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Que marca quieres buscar");
+        String marca = scanner.next();
+
+        System.out.println("Cuantos CV minimo quieres");
+        int cv = scanner.nextInt();
+        filtrarDatos(marca,cv);*/
     }
 
     private void iniciarLista(){
@@ -20,6 +24,7 @@ public class Ejercicio5 {
         // marca(str) modelo(str) cv(int) --> Coche
         Object[] coche1 = new Object[]{"Ford","Focus",150};
         Object[] coche4 = new Object[]{"Ford","Fiesta",90};
+        Object[] coche7 = new Object[]{"Ford","Mustang",300};
         Object[] coche2 = new Object[]{"Opel","Astra",100};
         Object[] coche5 = new Object[]{"Opel","Corsa",110};
         Object[] coche3 = new Object[]{"Seat","Leon",120};
@@ -30,17 +35,47 @@ public class Ejercicio5 {
         listaCoches.add(coche4);
         listaCoches.add(coche5);
         listaCoches.add(coche6);
+        listaCoches.add(coche7);
         System.out.println("Los cv del seat son "+listaCoches.get(2)[2]);
     }
 
-    public void mostrarCoches(){
+    private void mostrarCoches(){
         for (Object[] coche :listaCoches) {
             System.out.printf("%s %s %d\n",coche[0],coche[1],coche[2]);
         }
     }
 
-    public void filtrarDatos(){
+    private void filtrarDatos(String marca, int cv){
         // sacar todos los coches de una marca pedido por parametros
+        for (Object[] item : listaCoches ) {
+            if (item[0].toString().equalsIgnoreCase(marca) && (int)item[2]>cv
+            ){
+                System.out.printf("%s %s %d\n",item[0],item[1],item[2]);
+            }
+        }
+    }
+
+    private void filtrarDatos(String marca){
+        // sacar todos los coches de una marca pedido por parametros
+        for (Object[] item : listaCoches ) {
+            if (item[0].toString().equalsIgnoreCase(marca)){
+                System.out.printf("%s %s %d\n",item[0],item[1],item[2]);
+            }
+        }
+    }
+
+    private void reprogramarCoches(String marca, int cv){
+        for ( Object[] item: listaCoches) {
+            if(item[0].toString().equalsIgnoreCase(marca)){
+                item[2] = (int)item[2]+cv;
+            }
+        }
+
+        /*for (int i = 0; i < listaCoches.size(); i++) {
+            if(listaCoches.get(i)[0].toString().equalsIgnoreCase(marca)){
+                listaCoches.get(i)[2] = (int)listaCoches.get(i)[2]+cv;
+            }
+        }*/
     }
 
 }
