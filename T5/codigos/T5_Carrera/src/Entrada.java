0637fa7b9de08367bb72b88124ac87d4aec64412
Develop and System <developandsystem@gmail.com>
2023-12-20 11:24:49 +0100

    update

--- T5/codigos/T5_Carrera/src/Entrada.java
+++ T5/codigos/T5_Carrera/src/Entrada.java
@@ -5,20 +5,27 @@ import model.Coche;
 public class Entrada {
 
     public static void main(String[] args) {
-        Coche participante1 = new Coche("Participante1","1234A",60,3000);
-        Coche participante2 = new Coche("Participante2","2234A",60,3000);
-        Coche participante3 = new Coche("Participante3","3234A",60,3000);
-        Coche participante4 = new Coche("Participante4","4234A",60,3000);
-        Circuito ciruito1 = new Circuito("Imola",300,5);
-        Circuito ciruito2 = new Circuito("Jarama",600,8);
+        Coche participante1 = new Coche("Participante1","1234",70,3000);
+        Coche participante2 = new Coche("Participante2","2345",90,3000);
+        Coche participante3 = new Coche("Participante3","3456",100,3000);
+        Coche participante4 = new Coche("Participante4","4567",110,3000);
+        Coche participante5 = new Coche("Participante5","5678",80,3000);
+        Coche participante6 = new Coche("Participante6","6789",75,3000);
+        Circuito ciruito1 = new Circuito("Imola",200,7);
+
         Carrera carrera1 = new Carrera(ciruito1);
         carrera1.inscribirParticipante(participante1);
         carrera1.inscribirParticipante(participante2);
         carrera1.inscribirParticipante(participante3);
+        carrera1.inscribirParticipante(participante4);
+        carrera1.inscribirParticipante(participante5);
+        carrera1.inscribirParticipante(participante6);
 
-        //carrera1.setCircuito(ciruito2);
+
         carrera1.mostrarDatos();
         carrera1.mostrarParrilla();
+        carrera1.iniciarCarrera();
+        //carrera1.mostrarParrilla();
 
 
         // velocidad = 0
--- T5/codigos/T5_Carrera/src/controller/Carrera.java
+++ T5/codigos/T5_Carrera/src/controller/Carrera.java
@@ -4,6 +4,8 @@ import model.Circuito;
 import model.Coche;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Carrera {
 
@@ -11,8 +13,10 @@ public class Carrera {
     private Coche ganador;
     private ArrayList<Coche> participantes;
 
-    public Carrera(){}
-    public Carrera(Circuito circuito){
+    public Carrera() {
+    }
+
+    public Carrera(Circuito circuito) {
         this.circuito = circuito;
         // ganador = null
         this.participantes = new ArrayList<>();
@@ -25,12 +29,12 @@ public class Carrera {
     // Vueltas: XXX
     // Nº participantes: XXX
 
-    public void mostrarDatos(){
-        System.out.println("La carrera se corre en "+circuito.getNombre());
+    public void mostrarDatos() {
+        System.out.println("La carrera se corre en " + circuito.getNombre());
         System.out.println("Los datos de la carrera son");
-        System.out.println("KM: "+circuito.getKmTotales());
-        System.out.println("Vueltas: "+circuito.getVueltas());
-        System.out.println("Nº de participantes: "+participantes.size());
+        System.out.println("KM: " + circuito.getKmTotales());
+        System.out.println("Vueltas: " + circuito.getVueltas());
+        System.out.println("Nº de participantes: " + participantes.size());
     }
 
     // inscribir un participante
@@ -40,13 +44,53 @@ public class Carrera {
     // si un participante se inscribe -> Participante inscrito con existo
     // si no puede se inscribe -> Problema en la inscripcion
 
-    public void inscribirParticipante(Coche coche){
-        if (participantes.size()==20){
+
+    public void decidirGanador() {
+
+        ganador = participantes.get(0);
+        if (ganador.getKm() >= circuito.getKmTotales()) {
+            ganador = participantes.get(0);
+            System.out.printf("El ganador de la carrera es %s con %dkm\n",
+                    ganador.getNombre(), ganador.getKm());
+        } else {
+            System.out.println("No hay ganador");
+            ganador = null;
+        }
+
+    }
+
+    public void iniciarCarrera() {
+        for (int i = 0; i < circuito.getVueltas(); i++) {
+            System.out.println("Vuleta " + (i + 1));
+            // cada coche acelera
+            // participantes (todos los coches)
+            for (Coche item : participantes) {
+                item.acelerar((int) (Math.random() * 51) + 25);
+            }
+            mostrarParrilla();
+        }
+        System.out.println("Vueltas oficiales terminadas");
+        decidirGanador();
+
+        // si no hay ganador -> vuelta extra
+        // si hay ganador -> muestro datos
+        while (ganador == null) {
+            System.out.println("Vuelta extra!!!");
+            for (Coche item : participantes) {
+                item.acelerar((int) (Math.random() * 51) + 25);
+            }
+            decidirGanador();
+        }
+
+    }
+
+    public void inscribirParticipante(Coche coche) {
+        if (participantes.size() == 20) {
             System.out.println("Error en la inscripcion: no hay plazas");
         } else {
             // esta la matricula del coche que quiero agregar en la lista?
             // true o false
-            if (estaCoche(coche.getMatricula())!=null){
+            if (estaCoche(coche.getMatricula()) != null) {
                 System.out.println("Error en la inscripcion. Datos duplicados");
             } else {
                 participantes.add(coche);
@@ -55,12 +99,12 @@ public class Carrera {
         }
     }
 
-    private Coche estaCoche(String matricula){
+    private Coche estaCoche(String matricula) {
 
         Coche coincideCoche = null;
         // recorre la lista y pregunta por las matriculas
-        for ( Coche coche : this.participantes ) {
-            if (coche.getMatricula().equalsIgnoreCase(matricula)){
+        for (Coche coche : this.participantes) {
+            if (coche.getMatricula().equalsIgnoreCase(matricula)) {
                 coincideCoche = coche;
                 break;
             }
@@ -68,13 +112,13 @@ public class Carrera {
         return coincideCoche;
     }
 
-    public void descalificarParticipante(String matricula){
+    public void descalificarParticipante(String matricula) {
         // esta el coche en la lista?
         // coche
 
-        Coche cocheEncontrado= estaCoche(matricula);
+        Coche cocheEncontrado = estaCoche(matricula);
 
-        if (cocheEncontrado!=null){
+        if (cocheEncontrado != null) {
             participantes.remove(cocheEncontrado);
             System.out.println("El coche se ha borrado ok");
         } else {
@@ -88,9 +132,25 @@ public class Carrera {
         }*/
     }
 
-    public void mostrarParrilla(){
+    public void mostrarParrilla() {
+
+
+        // ordena la lista ->
+        Collections.sort(participantes, new Comparator<Coche>() {
+            @Override
+            public int compare(Coche o1, Coche o2) {
+                if (o1.getKm() < o2.getKm()) {
+                    return 1;
+                } else if (o1.getKm() > o2.getKm()) {
+                    return -1;
+                }
+                return 0;
+            }
+        });
+
         for (int i = 0; i < participantes.size(); i++) {
-            System.out.printf("%d %s\n",i+1,participantes.get(i).getNombre());
+            System.out.printf("%d - %s %d km\n", i + 1, participantes.get(i).getNombre()
+                    , participantes.get(i).getKm());
         }
 
         /*int posicion = 1;
