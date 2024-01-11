7a9286b4ae27f3dbb93d1d546cf090f646a4d3bb
Develop and System <developandsystem@gmail.com>
2024-01-11 10:36:56 +0100

    Update Entrada.java

--- T5/codigos/T5_Ejercicios/src/clase/ejercicio3/Entrada.java
+++ T5/codigos/T5_Ejercicios/src/clase/ejercicio3/Entrada.java
@@ -24,7 +24,7 @@ public class Entrada {
         //Alumno alumno1 = new Alumno(new Asignatura(asignaturas.get(0), new Asignatura(2, "Sistemas"), new Asignatura(3, "Base de datos"));
         Alumno alumno2 = new Alumno(new Asignatura(1, "Programacion"), new Asignatura(2, "Sistemas"), asignaturas.get(0));
         Profesor profesor = new Profesor("Borja");
-        profesor.ponerNotas(alumno1);
+        //profesor.ponerNotas(alumno1);
         //profesor.ponerNotas(alumno2);
         //alumno1.mostrarDatos();
         //System.out.println(profesor.calcularMedia(alumno)); // LA MEDIA
