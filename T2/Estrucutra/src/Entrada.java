c8d172da9f4692679dfa9d625b424b82b0b3cdbc
Develop and System <developandsystem@gmail.com>
2024-09-17 13:58:45 +0200

    update

---
+++ T2/Estrucutra/src/Entrada.java
@@ -0,0 +1,43 @@
+public class Entrada {
+
+    /*
+     Comentario
+     que sirve para poder
+     poner varias lineas de
+     explicacion
+    */
+    public static void main(String[] args) {
+
+        // TODO me falta por declarar las variables y tiparlas
+
+        // comentario de una linea
+        System.out.println("Hola, me llamo "+"Borja"+" y tengo "+40+" años y mido "+1.74);
+        // %s -> string
+        // %d -> int
+        // %.2f -> .numeroDecimales float
+        System.out.printf("Hola me llamo %s y tengo %d años y mido %.2f\n","Borja",1.84);
+        // String
+        System.out.println("Primer proyecto JAVA");
+        // String
+        System.out.println("Mi nombre es Borja");
+        // String
+        System.out.println("Tengo 40 años");
+        // char
+        System.out.println('a');
+        // int
+        System.out.println(5);
+        // float - double
+        System.out.println(4.123);
+        // boolean
+        System.out.println(false);
+        System.out.println(true);
+
+
+        // TODO  instalar plugin de corchetes
+
+    }
+
+
+}
+
+
