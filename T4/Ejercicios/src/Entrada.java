0cce76496c04771faae88564c116f45a101a915d
Develop and System <developandsystem@gmail.com>
2024-12-10 11:21:30 +0100

    update

---
+++ T4/Ejercicios/src/Entrada.java
@@ -0,0 +1,47 @@
+import controller.Agenda;
+
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Entrada {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Agenda agenda = new Agenda();
+        agenda.agregarPersona("Borja","martin",1234,"1234A");
+        // []
+        /*System.out.println("Por favor introduce tu nombre");
+        String nombre = scanner.next();
+        System.out.println("Por favor introduce tu apellido");
+        String apellido = scanner.next();
+        System.out.println("Por favor introduce tu telefono");
+        int telefono = scanner.nextInt();
+        System.out.println("Por favor introduce tu dni");
+        String dni = scanner.next();
+        if (agenda.agregarPersona(nombre,apellido,telefono,dni)){
+            // agregado
+            System.out.println("Usuario agregado correctamente");
+        } else {
+            //
+            System.out.println("Fallo en el proceso");
+        }*/
+        // si la lista esta vacia -> aviso
+        switch (agenda.listarDatosPersona("1234A")){
+            case 1:
+                System.out.println("No se pueden hacer busquedas sobre una lista vacia");
+                break;
+            case 2:
+                System.out.println("No esta en la lista, queres agregarlo (s/n)");
+                String  contestacion = scanner.next();
+                if (contestacion.equalsIgnoreCase("s")){
+                    // pido datos
+                    // agregar persona
+                    agenda.agregarPersona("Borja","Martin",1234,"1234B");
+                }else {
+                    System.out.println("Continuando con el programa");
+                }
+                break;
+        }
+    }
+}
---
+++ T4/Ejercicios/src/controller/Agenda.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Agenda {
+
+    private ArrayList<Object[]> listaContactos;
+
+    // constructor
+    // private static void nombre(params){}
+    public Agenda() {
+        listaContactos = new ArrayList<>();
+    }
+
+    public boolean agregarPersona(String nombre, String apellido,
+                                  int telefono, String dni) {
+
+        Object[] contacto = {nombre, apellido, telefono, dni};
+        // comprobacion dni
+        if (buscarUsuario(dni) == null) {
+            // agregar
+            listaContactos.add(contacto);
+            return true;
+        }
+        // no agregar la persona
+        return false;
+    }
+
+    private Object[] buscarUsuario(String dni) {
+
+        for (Object[] item : listaContactos) {
+            if (String.valueOf(item[3]).equalsIgnoreCase(dni)) {
+                // esta en la lista
+                return item;
+            }
+        }
+        // no esta en la lista
+        return null;
+    }
+
+    public int listarDatosPersona(String dni) {
+        Object[] usuario = buscarUsuario(dni);
+        if (listaContactos.isEmpty()) {
+            // la lista esta vacia
+            return 1;
+        } else if (usuario == null) {
+            // la lista no esta vacia pero el usuario no esta
+            return 2;
+        }
+        // la lista no esta vacia pero el usuario si esta
+        for ( Object item : usuario ) {
+            System.out.println(item);
+        }
+        return -1;
+
+    }
+
+
+}
