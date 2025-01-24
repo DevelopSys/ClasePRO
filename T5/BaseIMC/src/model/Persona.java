e2360fab589d5043bff020e0be4b2bf6361bc8a9
Develop and System <developandsystem@gmail.com>
2025-01-24 13:54:38 +0100

    update

---
+++ T5/BaseIMC/src/model/Persona.java
@@ -0,0 +1,103 @@
+package model;
+
+public class Persona {
+
+    private String nombre, apellido, dni;
+    private int edad;
+    private double altura, peso;
+
+    public Persona(String nombre, String apellido, String dni,
+                   int edad, double altura, double peso) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.edad = edad;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public Persona(String nombre, String apellido, String dni, int edad) {
+
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+
+    }
+
+    public Persona(String nombre, String apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = "11111111X";
+        // peso = 0.0
+        // altura = 0.0
+        // edad = 0
+    }
+
+    public Persona(){
+        this.nombre = "sin definir";
+        this.apellido = "sin definir";
+        this.dni = "1111111X";
+        // this.edad = 0;
+        // this.peso = 0;
+        // this.altura =0;
+    }
+
+    public void mostrarDatos() {
+        System.out.printf("Nombre: %s" +
+                        "\nApellido: %s" +
+                        "\nDNI: %s" +
+                        "\nAltura: %.2f"
+                        + "\nPeso: %.2f"
+                        + "\nEdad: %d",
+                nombre, apellido, dni, altura, peso, edad);
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+}
