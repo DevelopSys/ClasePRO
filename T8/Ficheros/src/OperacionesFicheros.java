108267fe7d0ccd72eef2d5cdafdc9499062b7330
Develop and System <developandsystem@gmail.com>
2025-03-25 10:35:35 +0100

    update

--- T8/Ficheros/src/OperacionesFicheros.java
+++ T8/Ficheros/src/OperacionesFicheros.java
@@ -57,16 +57,62 @@ public class OperacionesFicheros {
         File file = new File(path);
         // hago lectura
         FileReader fileReader = null;
+
         try {
             fileReader = new FileReader(file);
-            int lectura = fileReader.read();
+            int lectura = -1;
+            while ( (lectura = fileReader.read())!=-1 ){
+                System.out.println("lectura = " + lectura+" asociado a la letra: "+(char)lectura);
+                // System.out.print((char)lectura);
+            }
+            /*int lectura = fileReader.read();
             System.out.println("lectura = " + lectura+" asociado a la letra: "+(char)lectura);
             lectura = fileReader.read();
-            System.out.println("lectura = " + lectura+" asociado a la letra: "+(char)lectura);
+            System.out.println("lectura = " + lectura+" asociado a la letra: "+(char)lectura);*/
         } catch (FileNotFoundException e) {
             System.out.println("El fichero no existe");
         } catch (IOException e) {
             System.out.println("Error en la lectura");
+        } finally {
+            System.out.println("Terminando lectura");
+            try {
+                fileReader.close();
+            } catch (IOException | NullPointerException e) {
+                System.out.println("Error en el cierre del fichero");
+            }
+        }
+
+
+    }
+
+
+    public void lecturaCodigoUnitaria(String path) {
+        File file = new File(path);
+        // hago lectura
+        FileReader fileReader = null;
+
+        try {
+            fileReader = new FileReader(file);
+            int lectura = -1;
+            while ( (lectura = fileReader.read())!=-1 ){
+                System.out.println("lectura = " + lectura+" asociado a la letra: "+(char)lectura);
+                // System.out.print((char)lectura);
+            }
+            /*int lectura = fileReader.read();
+            System.out.println("lectura = " + lectura+" asociado a la letra: "+(char)lectura);
+            lectura = fileReader.read();
+            System.out.println("lectura = " + lectura+" asociado a la letra: "+(char)lectura);*/
+        } catch (FileNotFoundException e) {
+            System.out.println("El fichero no existe");
+        } catch (IOException e) {
+            System.out.println("Error en la lectura");
+        } finally {
+            System.out.println("Terminando lectura");
+            try {
+                fileReader.close();
+            } catch (IOException | NullPointerException e) {
+                System.out.println("Error en el cierre del fichero");
+            }
         }
 
 
