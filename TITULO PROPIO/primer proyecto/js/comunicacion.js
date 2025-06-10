57aca2827b02b84caa8c6e1e06495474fcce49ab
Develop and System <developandsystem@gmail.com>
2025-06-10 13:23:02 +0200

    update

---
+++ TITULO PROPIO/primer proyecto/js/comunicacion.js
@@ -0,0 +1,22 @@
+// alert -> feednack informativo
+// confirm -> feedback de confirmación
+// prompt -> feedback de entrada de datos
+let nombre = "Borja Martin";
+let edad = 41;
+// alert(`Primera pagina dinamica de ${nombre}`);
+/* let respuesta = confirm("Estas seguro de que quieres continuar?");
+
+if (respuesta) {
+  alert(`Bienvenido ${nombre} a la segunda parte del ejercicio `);
+} else {
+  alert("Has seleccionado que no quieres continuar");
+} */
+
+let nombrePrompt = prompt("Introduce tu nombre", "nombre random");
+console.log(nombrePrompt);
+
+/* PEdir por prompt un nombre */
+// una vez introducido, preguntar al usuario si quiere hacer opreaciones
+// en caso de que si, preguntar por operando 1 y opreando 2
+// mostrar por consola todos los resultados de las operaciones (+, - , * ,/)
+// En caso de que no, mostrar un mensaje de despedida
