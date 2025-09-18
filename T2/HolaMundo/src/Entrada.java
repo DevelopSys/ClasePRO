75321b0b51f12298aea0f01afa01964ee4b53240
Develop and System <developandsystem@gmail.com>
2025-09-18 12:51:36 +0200

    update

--- T2/HolaMundo/src/Entrada.java
+++ T2/HolaMundo/src/Entrada.java
@@ -1,5 +1,47 @@
 public class Entrada {
-    public static void main(String[] args){
 
+    // Este metodo es el principal
+    // mod_acceso mod_pertenecia retorno nombre(parametros){}
+    // esta linea es nueva porque la necesito
+
+    /**
+     * @param args
+     * @return
+     * @version
+     * @author
+     */
+    public static void main(String[] args) {
+
+        // palabras
+        String nombreCompleto = "Borja";
+        nombreCompleto = "Borja Martin";
+
+        // letras
+        char letraDNI = 'a';
+        letraDNI = 'b';
+        letraDNI = 123;
+        // boolean
+        boolean acierto = true;
+        acierto = false;
+        // numeros enteros
+        int edad = 41;
+        edad = 42;
+        // numeros con decimales
+        float altura = 1.80f;
+        double peso = 70.89;
+
+        System.out.println("Primer programa en JAVA");
+        // aqui tengo un error que creo que va por esta cosa
+        System.out.print("\t\tEsta es la segunda linea\n");
+        System.out.println("Mi nombre es "+nombreCompleto);
+        // System.out.println("Mi nombre es Borja Martin");
+        // todo tengo que declarar las variabes y reorganizarlas
     }
+
+    /*
+    Este comentario me vale para poder escribir un rato
+    sin necesidad de poner todo el rato
+    cuidado con este tipo de comentarios
+     */
+
 }
