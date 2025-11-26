5cddfe2b6b88e800a7645b9d53ba71b910e4a9d4
Develop and System <developandsystem@gmail.com>
2025-11-26 13:00:45 +0100

    update

--- T5/Objetos/src/Main.java
+++ T5/Objetos/src/Main.java
@@ -5,9 +5,16 @@ import java.lang.invoke.ConstantCallSite;
 public class Main {
 
     public static void main(String[] args) {
-        Vehiculo vehiculo = new Vehiculo("Ford","Focus");
-        // marca // modelo // ruedas // cv // cc // ruedas
-        Vehiculo vehiculo1 = new Vehiculo("Seat","Panda");
-        // marca // modelo // ruedas // cv // cc // ruedas
+        Vehiculo vehiculo0 = new Vehiculo();
+        Vehiculo vehiculo = new Vehiculo("Ford", "Focus");
+        // marca // modelo // ruedas // cv // cc // tipo
+        Vehiculo vehiculo1 = new Vehiculo("Seat", "Panda");
+        // marca // modelo // ruedas // cv // cc // tipo
+        Vehiculo vehiculo2 = new Vehiculo("Ford", "focus", "utilitario");
+        // marca // modelo // ruedas // cv // cc // tipo
+        Vehiculo vehiculo3 = new Vehiculo("Ford", "Focus",  "utilitario", 100,2000);
+        System.out.println("el modelo del coche es "+vehiculo1.getModelo());
+
+
     }
 }
--- T5/Objetos/src/model/Vehiculo.java
+++ T5/Objetos/src/model/Vehiculo.java
@@ -1,15 +1,50 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Vehiculo {
     // variables -> caracteristicas que cualifican el elemento
-    private String marca, modelo;
-    private int cc, cv, ruedas;
+    private String marca, modelo, tipo;
+    private int cc, cv;
+    private int ruedas;
+    private ArrayList<String> listaAccesorios;
 
     // constructor -> metodos especiales que permiten hacer
     // realizad los objetos de la clase
-    public Vehiculo(String marca, String modelo){
+    public Vehiculo() {
+        listaAccesorios = new ArrayList<>();
+    }
+
+    public Vehiculo(String marca, String modelo) {
+        listaAccesorios = new ArrayList<>();
         this.marca = marca;
         this.modelo = modelo;
+    }
+
+    public Vehiculo(String marca, String modelo, String tipo) {
+        listaAccesorios = new ArrayList<>();
+        this.marca = marca;
+        this.modelo = modelo;
+        this.tipo = tipo;
         this.ruedas = 4;
     }
+
+    public Vehiculo(String marca, String modelo, String tipo, int cv,
+                    int cc) {
+        listaAccesorios = new ArrayList<>();
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cc = cc;
+        this.cv = cv;
+        this.tipo = tipo;
+        this.ruedas = 0;
+    }
+
+
+    public String getModelo(){
+        // logica de comprobacion
+        return this.modelo;
+    }
+
+
 }
