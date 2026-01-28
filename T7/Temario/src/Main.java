d729974d1fe7b415cbfa455521e812efebaf8f27
Develop and System <developandsystem@gmail.com>
2026-01-28 09:19:09 +0100

    update

--- T7/Temario/src/Main.java
+++ T7/Temario/src/Main.java
@@ -2,6 +2,9 @@ import model.Asignatura;
 import model.Profesor;
 import model.Tipo;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -18,8 +21,19 @@ public class Main {
         Asignatura.Tema tema6 = asignatura3.new Tema("Tema2", 5, 3, 30);
         Asignatura.Tema tema7 = asignatura4.new Tema("Tema2", 5, 3, 30);
         Asignatura.Tema tema8 = asignatura2.new Tema("Tema2", 5, 3, 30);
-        asignatura1.listarTemario();
-        // Asignatura.Tema tema3 = asignatura2.new Tema("Tema1", 1, 1, 10);
-        // asignatura2.mostrarDatos();
+        ArrayList<Asignatura> asignaturas = new ArrayList<>();
+        // AS1 AS2 AS3 AS4
+        asignaturas.add(asignatura1);
+        asignaturas.add(asignatura2);
+        asignaturas.add(asignatura3);
+        asignaturas.add(asignatura4);
+        // asignaturas.forEach(Asignatura::listarTemario);
+        asignaturas.stream()
+                .map(Asignatura::getTemario)
+                .forEach(temas -> {
+                    temas.forEach(Asignatura.Tema::mostrarDatos);
+                });
+
+
     }
 }
