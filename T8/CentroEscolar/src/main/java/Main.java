76345d8b055f16dbe3cb59607a79e8bf61fbd222
Develop and System <developandsystem@gmail.com>
2026-03-12 12:15:54 +0100

    update

--- T8/CentroEscolar/src/main/java/Main.java
+++ T8/CentroEscolar/src/main/java/Main.java
@@ -4,19 +4,43 @@ import model.Alumno;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         AlumnoDTO alumnoDTO = new AlumnoDTO();
-        boolean resultado = alumnoDTO
-                .addUser(new Alumno("Jose","Garcia","jose@gmail.com",1234));
-        System.out.println(resultado);
-
+        // pido los datos por consola
+        /*Alumno alumno = new Alumno("Jose", "Garcia", "jose@gmail.com", 1234);
+        boolean resultado = false;
+        boolean fallo = false;
+        do {
+            try {
+                resultado = alumnoDTO
+                        .addUser(alumno);
+                fallo = false;
+            } catch (SQLException e) {
+                fallo = true;
+                System.out.println("Error en un mail duplicado, por favor introduce nuevamente el mail");
+                Scanner scanner = new Scanner(System.in);
+                String mail = scanner.next();
+                alumno.setCorreo(mail);
+            }
+        }while (fallo);
+        System.out.println("Usuario insertado correctamente");*/
         /*
         Crear un programa que pida por consola el nombde, apellido, correo y telefono del usuario
         En caso de insercion correcta -> sout("dato insertado correctamente")
         En caso de error en duplicidad -> indica un mail diferente, y lo vuelve a intentars
+         */
+        int row = alumnoDTO.addUserPS(new Alumno("Matias","Gomez","matias@gmail.com",23456));
+        System.out.println("El numero de filas afectadas es de: "+row);
+
+
+        /*
+        Crear un programa que permita indicar una localidad y cuanto sube la couta
+        // capturar todos los usuarios de esa localidad y subirles la cuota en lo que estipule lo indicado
+        MADRID, 500
          */
     }
 }
--- T8/CentroEscolar/src/main/java/dto/AlumnoDTO.java
+++ T8/CentroEscolar/src/main/java/dto/AlumnoDTO.java
@@ -17,22 +17,38 @@ public class AlumnoDTO {
     private PreparedStatement preparedStatement;
     private ResultSet resultSet;
 
-    public boolean addUser(Alumno alumno) {
+    public boolean addUser(Alumno alumno) throws SQLException {
         connection = DBConnector.getConnection();
+
+        statement = connection.createStatement();
+        String query = String.format("INSERT INTO %s (%s,%s,%s,%s) " +
+                        "VALUES ('%s','%s','%s',%d)",
+                SchemDB.TAB_USER,
+                SchemDB.COL_NAME, SchemDB.COL_SURNAME, SchemDB.COL_MAIL, SchemDB.COL_PHONE,
+                alumno.getNombre(), alumno.getApellido(), alumno.getCorreo(), alumno.getTelefono()
+        );
+        Boolean resultado = statement.execute(query);
+        return resultado;
+
+    }
+    public int addUserPS(Alumno alumno) {
+        connection = DBConnector.getConnection();
+        String query = String.format("INSERT INTO %s (%s,%s,%s,%s) " +
+                        "VALUES (?,?,?,?)",
+                SchemDB.TAB_USER,
+                SchemDB.COL_NAME, SchemDB.COL_SURNAME, SchemDB.COL_MAIL, SchemDB.COL_PHONE
+        );
         try {
-            statement = connection.createStatement();
-            String query = String.format("INSERT INTO %s (%s,%s,%s,%s) " +
-                    "VALUES ('%s','%s','%s',%d)",
-                    SchemDB.TAB_USER,
-                    SchemDB.COL_NAME,SchemDB.COL_SURNAME,SchemDB.COL_MAIL,SchemDB.COL_PHONE,
-                    alumno.getNombre(),alumno.getApellido(),alumno.getCorreo(),alumno.getTelefono()
-                    );
-            Boolean resultado =  statement.execute(query);
-            return resultado;
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1,alumno.getNombre());
+            preparedStatement.setString(2,alumno.getApellido());
+            preparedStatement.setString(3,alumno.getCorreo());
+            preparedStatement.setInt(4,alumno.getTelefono());
+            return preparedStatement.executeUpdate();
         } catch (SQLException e) {
-            System.out.println("Error en la creacion del ST");
+            System.out.println("Error en la query");
         }
 
-        return true;
+        return -1;
     }
 }
