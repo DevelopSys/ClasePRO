86cc88c2d811778c807c55f1a6b477488d2027d1
Develop and System <developandsystem@gmail.com>
2026-09-17 11:22:20 +0200

    update

---
+++ T2/Componentes/src/Entrada.java
@@ -0,0 +1,21 @@
+public class Entrada {
+
+    public static void main(String[] args){
+        // esta es la definicion de una variable
+        String nombre = "Borja";
+        String apellido1 = "Martin";
+        String apellido2 = "Herrera";
+        String correo = "borja.martin@cesjunapablosegundo.es";
+        System.out.print("Hola me llamo "+nombre+" "+apellido1+" "+apellido2+"\n");
+        System.out.print("\tMi correo es "+correo);
+
+
+    }
+
+
+}
+
+
+
+
+
